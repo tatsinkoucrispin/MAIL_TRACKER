70e2e64040e1122f5831c86429594524206243ba
unknown <tanko.leila123@gmail.com>
2024-03-13 20:07:52 +0100

    MAJ de BD

--- BD/gbov2.sql
+++ BD/gbov2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 05 mars 2024 à 17:51
--- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
+-- Généré le : mer. 13 mars 2024 à 18:59
+-- Version du serveur : 5.7.36
+-- Version de PHP : 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,11 +29,20 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `affecter`;
 CREATE TABLE IF NOT EXISTS `affecter` (
-  `ID_DEPARTEMENT` int DEFAULT NULL,
-  `N_ARRIVEE` int DEFAULT NULL,
+  `ID_DEPARTEMENT` int(11) DEFAULT NULL,
+  `N_ARRIVEE` int(11) DEFAULT NULL,
   KEY `ID_DEPARTEMENT` (`ID_DEPARTEMENT`),
   KEY `N_ARRIVEE` (`N_ARRIVEE`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `affecter`
+--
+
+INSERT INTO `affecter` (`ID_DEPARTEMENT`, `N_ARRIVEE`) VALUES
+(1, 4),
+(2, 4),
+(2, 5);
 
 -- --------------------------------------------------------
 
@@ -43,23 +52,27 @@ CREATE TABLE IF NOT EXISTS `affecter` (
 
 DROP TABLE IF EXISTS `courrier_arrivee`;
 CREATE TABLE IF NOT EXISTS `courrier_arrivee` (
-  `N_ARRIVEE` int NOT NULL,
-  `ANNEE` int DEFAULT NULL,
+  `N_ARRIVEE` int(11) NOT NULL,
+  `ANNEE` int(11) DEFAULT NULL,
   `OBJET_ARRIVEE` varchar(255) DEFAULT NULL,
   `DATE_ARRIVEE` date DEFAULT NULL,
   `PIECE_JOINTE_ARRIVEE` varchar(255) DEFAULT NULL,
   `REPONSE` text,
-  PRIMARY KEY (`N_ARRIVEE`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `ID_EXP` int(11) NOT NULL,
+  PRIMARY KEY (`N_ARRIVEE`),
+  KEY `fk_expediteur_id_exp` (`ID_EXP`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `courrier_arrivee`
 --
 
-INSERT INTO `courrier_arrivee` (`N_ARRIVEE`, `ANNEE`, `OBJET_ARRIVEE`, `DATE_ARRIVEE`, `PIECE_JOINTE_ARRIVEE`, `REPONSE`) VALUES
-(1, 2024, 'Réception d un colis', '2024-02-15', 'Facture.pdf', 'En cours de traitement'),
-(2, 2024, 'Demande de devis', '2024-02-16', 'Contrat.pdf', 'En attente'),
-(3, 2024, 'Confirmation de rendez-vous', '2024-02-17', 'Planning.pdf', 'Traité');
+INSERT INTO `courrier_arrivee` (`N_ARRIVEE`, `ANNEE`, `OBJET_ARRIVEE`, `DATE_ARRIVEE`, `PIECE_JOINTE_ARRIVEE`, `REPONSE`, `ID_EXP`) VALUES
+(1, 2024, 'Réception d un colis', '2024-02-15', 'Facture.pdf', 'En cours de traitement', 0),
+(2, 2024, 'Demande de devis', '2024-02-16', 'Contrat.pdf', 'En attente', 0),
+(3, 2024, 'Confirmation de rendez-vous', '2024-02-17', 'Planning.pdf', 'Traité', 0),
+(4, 2024, 'cahier charge', '2024-03-14', 'TANKO SIME Leïla Kaltoumi CV.pdf', 'Avec Reponse', 4),
+(5, 2024, 'Demande de stage professionnel', '2024-03-10', 'Campus France CSI et Esperts.xlsx', 'Avec Reponse', 2);
 
 -- --------------------------------------------------------
 
@@ -69,23 +82,28 @@ INSERT INTO `courrier_arrivee` (`N_ARRIVEE`, `ANNEE`, `OBJET_ARRIVEE`, `DATE_ARR
 
 DROP TABLE IF EXISTS `courrier_depart_ext`;
 CREATE TABLE IF NOT EXISTS `courrier_depart_ext` (
-  `N_DEPART_EXT` int NOT NULL,
-  `ANNEE` int DEFAULT NULL,
+  `N_DEPART_EXT` int(11) NOT NULL AUTO_INCREMENT,
+  `ANNEE` int(11) DEFAULT NULL,
   `OBJET_DEPART_EXT` varchar(255) DEFAULT NULL,
   `DATE_DEPART_EXT` date DEFAULT NULL,
   `PIECE_JOINTE_DEPART_EXT` varchar(255) DEFAULT NULL,
   `REPONSE` text,
-  PRIMARY KEY (`N_DEPART_EXT`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `ID_DEPARTEMENT` int(11) NOT NULL,
+  `ID_DES` int(11) NOT NULL,
+  PRIMARY KEY (`N_DEPART_EXT`),
+  KEY `fk_departement_id_departement` (`ID_DEPARTEMENT`),
+  KEY `fk_destinataire_id_des` (`ID_DES`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `courrier_depart_ext`
 --
 
-INSERT INTO `courrier_depart_ext` (`N_DEPART_EXT`, `ANNEE`, `OBJET_DEPART_EXT`, `DATE_DEPART_EXT`, `PIECE_JOINTE_DEPART_EXT`, `REPONSE`) VALUES
-(1, 2024, 'Demande de partenariat', '2024-02-15', 'Proposition.pdf', 'En cours de traitement'),
-(2, 2024, 'Réponse à une offre', '2024-02-16', 'Contrat.pdf', 'Accepté'),
-(3, 2024, 'Invitation à un événement', '2024-02-17', 'Programme.pdf', 'En attente');
+INSERT INTO `courrier_depart_ext` (`N_DEPART_EXT`, `ANNEE`, `OBJET_DEPART_EXT`, `DATE_DEPART_EXT`, `PIECE_JOINTE_DEPART_EXT`, `REPONSE`, `ID_DEPARTEMENT`, `ID_DES`) VALUES
+(1, 2024, 'Reponse à la demande d\'emploi', '2024-03-09', 'Campus France CSI et Esperts.xlsx', 'Oui', 2, 1),
+(2, 2024, 'Réponse à la demande de stage', '2024-03-10', 'ges_stock.sql', 'Non', 3, 2),
+(3, 2024, 'Réponse à demande stage professionnel', '2023-10-19', 'manifest.mf', 'Oui', 1, 1),
+(5, 2024, 'Réponse a la demande d\'aide de l\'orphelinat', '2024-03-09', 'build.xml', 'Oui', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -95,15 +113,15 @@ INSERT INTO `courrier_depart_ext` (`N_DEPART_EXT`, `ANNEE`, `OBJET_DEPART_EXT`, 
 
 DROP TABLE IF EXISTS `courrier_depart_int`;
 CREATE TABLE IF NOT EXISTS `courrier_depart_int` (
-  `N_DEPART_INT` int NOT NULL,
-  `ID_DEPARTEMENT` int DEFAULT NULL,
-  `ANNEE` int DEFAULT NULL,
+  `N_DEPART_INT` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_DEPARTEMENT` int(11) DEFAULT NULL,
+  `ANNEE` int(11) DEFAULT NULL,
   `OBJET_DAPART_INT` varchar(255) DEFAULT NULL,
   `DATE_DEPART_INT` date DEFAULT NULL,
   `PIECE_JOINTE_DEPART_INT` varchar(255) DEFAULT NULL,
   `REPONSE` text,
   PRIMARY KEY (`N_DEPART_INT`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `courrier_depart_int`
@@ -112,7 +130,8 @@ CREATE TABLE IF NOT EXISTS `courrier_depart_int` (
 INSERT INTO `courrier_depart_int` (`N_DEPART_INT`, `ID_DEPARTEMENT`, `ANNEE`, `OBJET_DAPART_INT`, `DATE_DEPART_INT`, `PIECE_JOINTE_DEPART_INT`, `REPONSE`) VALUES
 (1, 1, 2024, 'Demande de congé', '2024-02-15', 'Formulaire.pdf', 'En attente'),
 (2, 1, 2024, 'Rapport de réunion', '2024-02-16', 'Notes.pdf', 'Traité'),
-(3, 2, 2024, 'Plan marketing', '2024-02-17', 'Présentation.pdf', 'En cours de traitement');
+(3, 2, 2024, 'Plan marketing', '2024-02-17', 'Présentation.pdf', 'En cours de traitement'),
+(4, 2, 2024, 'demande de congés', '2024-03-15', 'Bdcollege.SQL', 'Non');
 
 -- --------------------------------------------------------
 
@@ -122,11 +141,11 @@ INSERT INTO `courrier_depart_int` (`N_DEPART_INT`, `ID_DEPARTEMENT`, `ANNEE`, `O
 
 DROP TABLE IF EXISTS `departement`;
 CREATE TABLE IF NOT EXISTS `departement` (
-  `ID_DEPARTEMENT` int NOT NULL,
+  `ID_DEPARTEMENT` int(11) NOT NULL AUTO_INCREMENT,
   `NOM_DEPARTEMENT` varchar(255) DEFAULT NULL,
   `RESPONSABLE` text,
   PRIMARY KEY (`ID_DEPARTEMENT`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `departement`
@@ -135,7 +154,9 @@ CREATE TABLE IF NOT EXISTS `departement` (
 INSERT INTO `departement` (`ID_DEPARTEMENT`, `NOM_DEPARTEMENT`, `RESPONSABLE`) VALUES
 (1, 'Ressources Humaines', 'John Doe'),
 (2, 'Marketing', 'Jane Smith'),
-(3, 'Finance', 'Alice Johnson');
+(3, 'Finance', 'Alice Johnson'),
+(4, 'Maintenance Informatique', 'Njaha Nathan');
+
 -- --------------------------------------------------------
 
 --
@@ -144,13 +165,13 @@ INSERT INTO `departement` (`ID_DEPARTEMENT`, `NOM_DEPARTEMENT`, `RESPONSABLE`) V
 
 DROP TABLE IF EXISTS `destinataire`;
 CREATE TABLE IF NOT EXISTS `destinataire` (
-  `ID_DES` int NOT NULL,
+  `ID_DES` int(11) NOT NULL AUTO_INCREMENT,
   `NOM_DES` varchar(255) DEFAULT NULL,
   `ADRESSE_DES` varchar(255) DEFAULT NULL,
   `TEL_DES` text,
   `FAX_DES` text,
   PRIMARY KEY (`ID_DES`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `destinataire`
@@ -159,8 +180,19 @@ CREATE TABLE IF NOT EXISTS `destinataire` (
 INSERT INTO `destinataire` (`ID_DES`, `NOM_DES`, `ADRESSE_DES`, `TEL_DES`, `FAX_DES`) VALUES
 (1, 'Mairie de Ville 1', '1 Rue de la Mairie, 75001 Ville 1', '01 23 45 67 89', '01 23 45 67 88'),
 (2, 'Entreprise A', '123 Rue de l Entreprise, 75002 Ville 2', '01 23 45 67 90', '01 23 45 67 91'),
-(3, 'Organisation B', '456 Avenue de l Organisation, 75003 Ville 3', '01 23 45 67 92', '01 23 45 67 93');
-
+(3, 'Organisation B', '456 Avenue de l Organisation, 75003 Ville 3', '01 23 45 67 92', '01 23 45 67 93'),
+(4, 'Entreprise A', 'Douala bonamoussadi', '65544823', '654785249'),
+(5, 'Entreprise A', 'Douala bonamoussadi', '65544823', '654785249'),
+(6, 'Entreprise A', 'Douala bonamoussadi', '65544823', '654785249'),
+(7, 'Organisation B', '456 Avenue de l Organisation, 7500 Ville 3', '0123456792', '01236793'),
+(8, 'Organisation B', '456 Avenue de l Organisation, 7500 Ville 3', '0123456792', '01236793'),
+(9, 'Entreprise A', 'Douala bonamoussadi', '65544823', '654785249'),
+(10, 'Entreprise A', 'Douala bonamoussadi', '65544823', '654785249'),
+(11, 'Mairie de Ville 1', '1 Rue de la Mairie, 75002 Ville 2', '0123456789', '01236788'),
+(12, 'Entreprise A', 'Douala bonamoussadi', '65544823', '654785249'),
+(13, 'Mairie de Ville 1', 'Douala bonamoussadi', '65544823', '654785249'),
+(14, 'Mairie de Ville 1', '1 Rue de la Mairie, 75001 Ville 1', '0123456789', '0123456788'),
+(15, 'Mairie de Ville 1', '1 Rue de la mairie, 75001 Ville 1', '0123456789', '0123456788');
 
 -- --------------------------------------------------------
 
@@ -170,13 +202,13 @@ INSERT INTO `destinataire` (`ID_DES`, `NOM_DES`, `ADRESSE_DES`, `TEL_DES`, `FAX_
 
 DROP TABLE IF EXISTS `expediteur`;
 CREATE TABLE IF NOT EXISTS `expediteur` (
-  `ID_EXP` int NOT NULL,
+  `ID_EXP` int(11) NOT NULL AUTO_INCREMENT,
   `NOM_EXP` varchar(255) DEFAULT NULL,
   `ADRESSE_EXP` varchar(255) DEFAULT NULL,
   `TEL_EXP` text,
   `FAX_EXP` text,
   PRIMARY KEY (`ID_EXP`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `expediteur`
@@ -185,8 +217,14 @@ CREATE TABLE IF NOT EXISTS `expediteur` (
 INSERT INTO `expediteur` (`ID_EXP`, `NOM_EXP`, `ADRESSE_EXP`, `TEL_EXP`, `FAX_EXP`) VALUES
 (1, 'Société XYZ', '789 Boulevard de la Société, 75004 Ville 4', '01 23 45 67 94', '01 23 45 67 95'),
 (2, 'Association C', '101 Rue de l Association, 75005 Ville 5', '01 23 45 67 96', '01 23 45 67 97'),
-(3, 'Particulier D', '111 Avenue du Particulier, 75006 Ville 6', '01 23 45 67 98', '01 23 45 67 99');
-
+(3, 'Particulier D', '111 Avenue du Particulier, 75006 Ville 6', '01 23 45 67 98', '01 23 45 67 99'),
+(4, 'Djouda Patric', 'Douala bonamoussadi', '515515555455155', '3655478'),
+(17, 'Association C', '101 Rue de l Association, 75005 Ville 5', '01 23 45 67 96', '01 23 45 67 97'),
+(16, 'Djouda Patric', 'Douala bonamoussadi', '515515555455155', '3655478'),
+(15, 'Djouda Patric', 'Douala bonamoussadi', '515515555455155', '3655478'),
+(14, 'Djouda Patric', 'Douala bonamoussadi', '515515555455155', '3655478'),
+(13, 'Djouda Patric', 'Douala bonamoussadi', '515515555455155', '3655478'),
+(12, 'Djouda Patric', 'Douala bonamoussadi', '515515555455155', '3655478');
 
 -- --------------------------------------------------------
 
@@ -196,15 +234,15 @@ INSERT INTO `expediteur` (`ID_EXP`, `NOM_EXP`, `ADRESSE_EXP`, `TEL_EXP`, `FAX_EX
 
 DROP TABLE IF EXISTS `personnel`;
 CREATE TABLE IF NOT EXISTS `personnel` (
-  `ID_PERSONNEL` int NOT NULL,
-  `ID_DEPARTEMENT` int DEFAULT NULL,
+  `ID_PERSONNEL` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_DEPARTEMENT` int(11) DEFAULT NULL,
   `NOM_PERSONNEL` varchar(255) DEFAULT NULL,
   `ADRESSE_PERSONNEL` text,
   `TEL_PERSONNEL` text,
   `NOM_DEPARTEMENT` text,
   PRIMARY KEY (`ID_PERSONNEL`),
   KEY `ID_DEPARTEMENT` (`ID_DEPARTEMENT`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `personnel`
@@ -215,8 +253,8 @@ INSERT INTO `personnel` (`ID_PERSONNEL`, `ID_DEPARTEMENT`, `NOM_PERSONNEL`, `ADR
 (2, 1, 'Marie Martin', '3 Rue Marie Martin, 75008 Ville 8', '01 23 45 67 01', 'Département Administratif'),
 (3, 2, 'Paul Dubois', '4 Rue Paul Dubois, 75009 Ville 9', '01 23 45 67 02', 'Département Technique'),
 (4, 2, 'Sophie Durand', '5 Rue Sophie Durand, 75010 Ville 10', '01 23 45 67 03', 'Département Technique'),
-(5, 3, 'Pierre Moreau', '6 Rue Pierre Moreau, 75011 Ville 11', '01 23 45 67 04', 'Département Commercial');
-
+(5, 3, 'Pierre Moreau', '6 Rue Pierre Moreau, 75011 Ville 11', '01 23 45 67 04', 'Département Commercial'),
+(6, 3, 'Noupa', 'Sorelle', '699332458', NULL);
 
 -- --------------------------------------------------------
 
@@ -226,11 +264,11 @@ INSERT INTO `personnel` (`ID_PERSONNEL`, `ID_DEPARTEMENT`, `NOM_PERSONNEL`, `ADR
 
 DROP TABLE IF EXISTS `recevoir_interne`;
 CREATE TABLE IF NOT EXISTS `recevoir_interne` (
-  `N_DEPART_INT` int DEFAULT NULL,
-  `ID_PERSONNEL` int DEFAULT NULL,
+  `N_DEPART_INT` int(11) DEFAULT NULL,
+  `ID_PERSONNEL` int(11) DEFAULT NULL,
   KEY `N_DEPART_INT` (`N_DEPART_INT`),
   KEY `ID_PERSONNEL` (`ID_PERSONNEL`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `recevoir_interne`
@@ -239,7 +277,30 @@ CREATE TABLE IF NOT EXISTS `recevoir_interne` (
 INSERT INTO `recevoir_interne` (`N_DEPART_INT`, `ID_PERSONNEL`) VALUES
 (1, 2),
 (2, 3),
-(3, 1);
+(3, 1),
+(4, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `transmettre`
+--
+
+DROP TABLE IF EXISTS `transmettre`;
+CREATE TABLE IF NOT EXISTS `transmettre` (
+  `ID_PERSONNEL` int(11) NOT NULL,
+  `N_ARRIVEE` int(11) NOT NULL,
+  PRIMARY KEY (`ID_PERSONNEL`,`N_ARRIVEE`),
+  KEY `fk_transmettre_courrier_arrivee` (`N_ARRIVEE`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `transmettre`
+--
+
+INSERT INTO `transmettre` (`ID_PERSONNEL`, `N_ARRIVEE`) VALUES
+(4, 4),
+(4, 5);
 
 -- --------------------------------------------------------
 
@@ -249,20 +310,25 @@ INSERT INTO `recevoir_interne` (`N_DEPART_INT`, `ID_PERSONNEL`) VALUES
 
 DROP TABLE IF EXISTS `transmettre3`;
 CREATE TABLE IF NOT EXISTS `transmettre3` (
-  `N_DEPART_INT` int DEFAULT NULL,
-  `ID_PERSONNEL` int DEFAULT NULL,
+  `N_DEPART_INT` int(11) DEFAULT NULL,
+  `ID_PERSONNEL` int(11) DEFAULT NULL,
+  `N_DEPART_EXT` int(11) NOT NULL,
   KEY `N_DEPART_INT` (`N_DEPART_INT`),
-  KEY `ID_PERSONNEL` (`ID_PERSONNEL`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `ID_PERSONNEL` (`ID_PERSONNEL`),
+  KEY `fk_n_depart_ext` (`N_DEPART_EXT`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `transmettre3`
 --
 
-INSERT INTO `transmettre3` (`N_DEPART_INT`, `ID_PERSONNEL`) VALUES
-(1, 1),
-(2, 2),
-(3, 3);
+INSERT INTO `transmettre3` (`N_DEPART_INT`, `ID_PERSONNEL`, `N_DEPART_EXT`) VALUES
+(1, 1, 0),
+(2, 2, 0),
+(3, 3, 0),
+(4, 3, 0),
+(NULL, NULL, 5),
+(NULL, 5, 5);
 
 -- --------------------------------------------------------
 
@@ -272,12 +338,12 @@ INSERT INTO `transmettre3` (`N_DEPART_INT`, `ID_PERSONNEL`) VALUES
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `IDCOMPTE` int NOT NULL,
+  `IDCOMPTE` int(11) NOT NULL AUTO_INCREMENT,
   `NOMUTILISATEUR` varchar(255) DEFAULT NULL,
-  `MOTDEPASSE` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `MOTDEPASSE` varchar(60) DEFAULT NULL,
   `TYPECOMPTE` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`IDCOMPTE`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `utilisateur`
